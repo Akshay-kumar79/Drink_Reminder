93758558aacc6a87269945bd80ec1261cd0b8d76
Akshay kumar shaw <akshay.8459361606@gmail.com>
2023-08-10 12:22:18 +0530

    1. removed "org-jetbrains-kotlin-jvm" plugin as not in used 2. changed Java version 17 to 11

--- build-logic/convention/src/main/kotlin/com/akshaw/convention/Android.kt
+++ build-logic/convention/src/main/kotlin/com/akshaw/convention/Android.kt
@@ -34,14 +34,14 @@ fun CommonExtension<*, *, *, *>.buildFeatures() {
 
 fun CommonExtension<*, *, *, *>.compileOptions() {
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 }
 
 fun CommonExtension<*, *, *, *>.kotlinOptions() {
     (this as ExtensionAware).extensions.configure<KotlinJvmOptions>(Android.Extension.KOTLIN_OPTIONS) {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 }
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -8,5 +8,4 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.hilt.android) apply false
-    alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
 }
