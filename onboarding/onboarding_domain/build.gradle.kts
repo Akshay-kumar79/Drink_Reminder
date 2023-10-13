516b2f4965521ea0d7848c92374195af8a6f779b
Akshay kumar shaw <akshay.8459361606@gmail.com>
2023-10-13 17:04:25 +0530

    Removed unused dependencies from onboarding-domain module

--- onboarding/onboarding_domain/build.gradle.kts
+++ onboarding/onboarding_domain/build.gradle.kts
@@ -18,36 +18,13 @@ dependencies {
     // Module
     implementation(project(Projects.Implementation.CORE))
     
-    // Android Materials
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.material3)
-    
-    // Compose
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    
-    //Navigation
-    implementation(libs.androidx.navigation.compose)
-    
     // Dagger - Hilt
     implementation(libs.dagger.hilt.android)
     kapt(libs.dagger.hilt.android.compiler)
     implementation(libs.dagger.hilt.navigation.compose)
     
-    // Room
-    implementation(libs.androidx.room.runtime)
-    kapt(libs.androidx.room.compiler)
-    implementation(libs.androidx.room.ktx)
-    
-    // Number picker
-    implementation(libs.number.picker)
-    
     // Local unit tests
     testImplementation(libs.bundles.test)
     testImplementation(project(Projects.TestImplementation.CORE_TEST))
     
-    // Debug implementations
-    debugImplementation(libs.androidx.ui.tooling)
 }
