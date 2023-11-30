1e4e6af526536452983e4faa695a20617c1cf65a
Akshay kumar shaw <akshay.8459361606@gmail.com>
2023-11-30 16:00:43 +0530

    Fixed problem:- Bottom sheet(in WaterReminderScreen to upsert reminders) not showing full layout by default in some devices.

--- water/water_presentation/src/main/java/com/akshaw/drinkreminder/waterpresentation/reminders/dialogs/UpsertReminderDialog.kt
+++ water/water_presentation/src/main/java/com/akshaw/drinkreminder/waterpresentation/reminders/dialogs/UpsertReminderDialog.kt
@@ -18,6 +18,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -78,7 +79,10 @@ fun UpsertReminderDialog(
     onMinuteChange: (minute: Int) -> Unit,
     onDaySelectionChange: (dayOfWeek: DayOfWeek) -> Unit
 ) {
+    val sheetState = rememberModalBottomSheetState(true)
+    
     ModalBottomSheet(
+        sheetState = sheetState,
         onDismissRequest = { onCancel() },
         dragHandle = null
     ) {
